1f8b43c1d1b60e6760cba9edd52b9679a9502c7c
Mihajlo <m.pavloski07@gmail.com>
2019-02-26 17:04:42 +0000

    Adding projects

--- mpav/__init__.py
+++ mpav/__init__.py
@@ -34,6 +34,7 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
+    # Register projects blueprint
     from . import projects
     app.register_blueprint(projects.bp)
 
--- mpav/projects.py
+++ mpav/projects.py
@@ -3,6 +3,7 @@ from mpav.db import get_db
 from flask import (
     g, render_template, request, session, url_for, Blueprint
 )
+from werkzeug.exceptions import abort
 
 bp = Blueprint('projects', __name__)
 
@@ -17,3 +18,24 @@ def get_projects():
     ).fetchall()
     print('num projects:', len(projects))
     return render_template('projects.html', projects=projects)
+
+
+def get_project(id_):
+
+    project = get_db().execute(
+        'SELECT id, title, ldesc from project where id = ?', (id_,)
+    ).fetchone()
+
+    if project is None:
+        abort(404, "Project id {0} does not exist".format(id_))
+
+    return project
+
+
+@bp.route('/project/<int:id>', methods=('GET', 'POST'))
+def show(id):
+    project = get_project(id)
+
+    return render_template('project_template.html', project=project)
+
+
